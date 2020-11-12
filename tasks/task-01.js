7fce9da6e90093d87677070c4e65da6215991405
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-13 00:06:36 +0200

    Create task-01

--- tasks/task-01.js
+++ tasks/task-01.js
@@ -1,19 +1,21 @@
 const categoryRef = document.querySelectorAll('.item');
 console.log(categoryRef);
 
-
-
 const computeList = (array) => `В списке ${array.length} категории.`;
 console.log(computeList(categoryRef));
 
-// const nameCategory = category.querySelector('h2');
-// const contItem = category.querySelectorAll('li');
-// console.log(computeList(contItem));
+// const categories = document.querySelectorAll('li.item');
+// categories.forEach((category) => {
+//   const title = category.querySelector('h2');
+//   ...
+
+//   const categories = document.querySelectorAll('li.item');
+categoryRef.forEach((category) => {
+    const title = category.querySelector('h2');
+    const contItem = category.querySelectorAll('li');
+
+    return console.log(`Категория: ${title.textContent}
+Количество элементов: ${contItem.length}`)
 
 
-const nameCategory = categoryRef.forEach(category => category.querySelector('h2') + category.querySelectorAll('li').length);
-// const contItem = categoryRef.forEach(category => category.querySelectorAll('li').length);
-console.log(nameCategory(categoryRef));
-
-// console.log(nameCategory);
-// console.log(contItem);
+})
