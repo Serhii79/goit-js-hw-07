3f26845def488289e5263b54cc66db9fdf68bc77
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-16 16:46:21 +0200

    Create task-04

--- tasks/task-04.js
+++ tasks/task-04.js
@@ -1,10 +1,26 @@
-const printMessage = function(message) {
-  console.log(message);
-};
 
-const higherOrderFunction = function(callback) {
-  const string = 'HOCs are awesome';
-  callback(string);
-};
+const spanValueRef = document.querySelector('span#value');
+const decrementRef = spanValueRef.previousSibling.previousSibling;
+const incrementRef = spanValueRef.nextSibling.nextSibling;
 
-higherOrderFunction(printMessage);
+// console.log(decrementRef);
+// console.log(incrementRef);
+// console.log(spanValueRef);
+
+
+let counterValue = 0;
+function computeValue() {
+    spanValueRef.textContent = counterValue;
+}
+function decrement() {
+    counterValue -= 1;
+    computeValue();
+}
+function increment() {
+    counterValue += 1;
+    computeValue();
+}
+
+
+decrementRef.addEventListener('click', decrement);
+incrementRef.addEventListener('click', increment);
