27e858b4823e2cc7d8ad1bd8488877bd0469cb41
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-18 00:29:50 +0200

    Create task-06

---
+++ tasks/task-06.js
@@ -0,0 +1,27 @@
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+const validationInputRef = document.querySelector('input#validation-input');
+console.dir(validationInputRef);
+
+validationInputRef.style.border = '3px solid #bdbdbd';
+
+
+validationInputRef.addEventListener('blur', e => {
+    if (Number(e.target.value.length) === Number(validationInputRef.dataset.length)) {
+    validationInputRef.style.borderColor = '#4caf50';
+} else {
+    validationInputRef.style.borderColor = '#f44336';
+}
+    // console.log('Hello');
+});
+
