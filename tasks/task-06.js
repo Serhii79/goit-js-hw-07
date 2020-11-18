f24b275cd40e2083cf6026f24eebc252a51a0817
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-19 00:34:02 +0200

    Change task-06

--- tasks/task-06.js
+++ tasks/task-06.js
@@ -11,16 +11,20 @@
 // }
 
 const validationInputRef = document.querySelector('input#validation-input');
-console.dir(validationInputRef);
+// console.dir(validationInputRef);
 
-validationInputRef.style.border = '3px solid #bdbdbd';
+// validationInputRef.style.border = '3px solid #bdbdbd';
 
 
 validationInputRef.addEventListener('blur', e => {
     if (Number(e.target.value.length) === Number(validationInputRef.dataset.length)) {
-    validationInputRef.style.borderColor = '#4caf50';
+        // validationInputRef.style.borderColor = '#4caf50';
+        validationInputRef.classList.add('valid');
+        validationInputRef.classList.remove('invalid');
 } else {
-    validationInputRef.style.borderColor = '#f44336';
+    // validationInputRef.style.borderColor = '#f44336';
+        validationInputRef.classList.add('invalid');
+        validationInputRef.classList.remove('valid');
 }
     // console.log('Hello');
 });
