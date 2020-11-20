d48d35f141588223be324f02a720f453fe7371ae
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-20 15:56:24 +0200

    Change task-03

--- tasks/task-03.js
+++ tasks/task-03.js
@@ -16,38 +16,34 @@ const images = [
   },
 ];
 
-const createListImg = document.querySelector('#gallery');
+const galleryRef = document.querySelector('#gallery');
 
-const list1 = document.createElement('li');
-const imageLink1 = document.createElement('img');
-list1.append(imageLink1);
-imageLink1.setAttribute('src', images[0].url);
-imageLink1.setAttribute('atl', images[0].alt);
+// const list1 = document.createElement('li');
+// const imageLink1 = document.createElement('img');
+// list1.append(imageLink1);
+// imageLink1.setAttribute('src', images[0].url);
+// imageLink1.setAttribute('atl', images[0].alt);
 
-const list2 = document.createElement('li');
-const imageLink2 = document.createElement('img');
-list2.append(imageLink2);
-imageLink2.setAttribute('src', images[1].url);
-imageLink2.setAttribute('atl', images[1].alt);
+// const list2 = document.createElement('li');
+// const imageLink2 = document.createElement('img');
+// list2.append(imageLink2);
+// imageLink2.setAttribute('src', images[1].url);
+// imageLink2.setAttribute('atl', images[1].alt);
 
-const list3 = document.createElement('li');
-const imageLink3 = document.createElement('img');
-list3.append(imageLink3);
-imageLink3.setAttribute('src', images[2].url);
-imageLink3.setAttribute('atl', images[2].alt);
+// const list3 = document.createElement('li');
+// const imageLink3 = document.createElement('img');
+// list3.append(imageLink3);
+// imageLink3.setAttribute('src', images[2].url);
+// imageLink3.setAttribute('atl', images[2].alt);
 
-createListImg.append(list1, list2, list3);
+// createListImg.append(list1, list2, list3);
 
-// images.forEach(item => {
-//     // console.log(item);
+const imagesList = images.map(item => {
 
-//     const list = document.createElement('li');
-//     const imageLink = document.createElement('img');
-//     // imageLink.setAtribute('src', item.url);
-//     // imageLink.setAtribute('alt', item.alt);
-//     list.append(imageLink);
-    
-    
-//     createListImg.append(list);
+  const list = document.createElement('li');
+  // list.insertAdjacentHTML('beforeend', '<img src=${item.url} alt=${item.alt} />');
+  list.appendChild(item);
+  // list.append(imageLink);
+});
 
-// })
+galleryRef.append(...imagesList);
